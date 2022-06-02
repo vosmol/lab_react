9aa5f7f1e71da1fb75bda7c035f9d2d7242bc5e1
vosmol <76426556+vosmol@users.noreply.github.com>
2022-06-02 18:12:46 +0200

    feat: Added hooks and R17 for comparison

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # About
 
-This repository is my refresher/exploration of React features with new ones from React18 included
+This repository is my refresher of React features and exploration of new ones added in React 18
 
 # Repo structure
 
 - Packages - source code
   - ⬜ General - standard React features
+    - ✅ Hooks
+    - ✅ Suspense - lazy + suspended fetch
     - ⬜ Context
     - ⬜ Dom - react-dom
-    - ⬜ Hooks
-    - ✅ Suspense - lazy + suspended fetch
     - ⬜ Other - rest of the features not included in other sections
+    - ⬜ Classes
   - ⬜ Renderer - custom React renderer
   - ⬜ SSR - server specific code to render react in node.js
+  - ⬜ Test package with React 17 for comparison
---
+++ packages/react-17/src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+export function App() {
+	return (
+		<div className="App">
+			<UseState />
+		</div>
+	);
+}
+
+const UseState = () => {
+	const [MM, setMin] = useState('--');
+	const [HH, setHour] = useState('--');
+	const [SS, setSec] = useState('--');
+
+	useEffect(() => {
+		function updateClock() {
+			const now = new Date();
+
+			setMin(('00' + now.getMinutes()).slice(-2));
+			setHour(('00' + now.getHours()).slice(-2));
+			setSec(('00' + now.getSeconds()).slice(-2));
+		}
+
+		const clock = setInterval(() => {
+			updateClock();
+		}, 5000);
+
+		updateClock();
+
+		return function () {
+			clearInterval(clock);
+		};
+	}, []);
+
+	console.log('clock rerendered');
+
+	return (
+		<div>
+			<h4>useState</h4>
+			<p>
+				{HH}:{MM}:{SS}
+			</p>
+		</div>
+	);
+};
