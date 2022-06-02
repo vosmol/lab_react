ba73be2ca5a8c13b9192bb7d2e14091fb129a091
vosmol <76426556+vosmol@users.noreply.github.com>
2022-06-02 16:53:12 +0200

    feat: Implemented suspense section

--- packages/general/src/sections/suspense.tsx
+++ packages/general/src/sections/suspense.tsx
@@ -1,7 +1,117 @@
-import { Suspense } from 'react';
-
-const keep = [Suspense];
+import { lazy, Suspense, useState } from 'react';
 
 export const Section_Suspense = () => {
-	return null;
+	const [mount, setMount] = useState(false);
+
+	return (
+		<div style={{ padding: '2rem' }}>
+			<h1>Dashboard</h1>
+			<button onClick={() => setMount((v) => !v)}>
+				{mount ? 'Unmount' : 'Mount'}
+			</button>
+			{mount ? (
+				<>
+					<Suspense fallback={<p>Loading the lazy one</p>}>
+						<LazyComponent />
+					</Suspense>
+					<div>
+						<Suspense fallback={<p>Loading ...</p>}>
+							<Activity />
+						</Suspense>
+					</div>
+					<div>
+						<Suspense fallback={<p>Generating graph ...</p>}>
+							<Graph />
+							<Suspense fallback={<p>Generating summary ...</p>}>
+								<GraphSummary />
+							</Suspense>
+						</Suspense>
+					</div>
+				</>
+			) : (
+				<p>Nothing to see here</p>
+			)}
+		</div>
+	);
 };
+
+const LazyComponent = lazy(
+	() =>
+		new Promise((res) => {
+			setTimeout(() => {
+				//@ts-ignore
+				res({ default: Lazy });
+			}, 3000);
+		})
+);
+
+const Lazy = () => {
+	return <p>Im sooooo lazy</p>;
+};
+
+const fetchActivity = createFetch({
+	items: [
+		{ id: 1, name: 'Ordered 20 items' },
+		{ id: 2, name: 'Canceled order' }
+	]
+});
+
+const Activity = () => {
+	const activitiy = fetchActivity();
+	return (
+		<div>
+			<h3>Activity</h3>
+			{activitiy.items.map(({ id, name }) => (
+				<div key={id}>{name}</div>
+			))}
+		</div>
+	);
+};
+
+const fetchGraph = createFetch({ points: [4, 19, 9, 8] });
+
+const Graph = () => {
+	const graph = fetchGraph();
+	return (
+		<div>
+			<h3>Graph</h3>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				{graph.points.map((p, i) => (
+					<span key={i}>{'-'.repeat(p)}</span>
+				))}
+			</div>
+		</div>
+	);
+};
+
+const fetchSummary = createFetch({ total: 10 }, 3000);
+
+const GraphSummary = () => {
+	const summary = fetchSummary();
+	return (
+		<div>
+			<h3>Graph summary</h3>
+			<div>Total: {summary.total}</div>
+		</div>
+	);
+};
+
+function createFetch<T>(data: T, time = 1000) {
+	let result: T;
+	let promise: Promise<T>;
+
+	return function () {
+		if (result) return result;
+
+		if (promise === undefined) {
+			promise = new Promise<T>((res) => {
+				setTimeout(() => {
+					result = data;
+					res(data);
+				}, time);
+			});
+		}
+
+		throw promise;
+	};
+}
