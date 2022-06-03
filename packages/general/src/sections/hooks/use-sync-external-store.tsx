76fcbdf1d203ff0a8eb8788bdec0db491d7a3838
vosmol <76426556+vosmol@users.noreply.github.com>
2022-06-03 16:58:46 +0200

    feat: Implemented useTransition and split externalSync

--- packages/general/src/sections/hooks/use-sync-external-store.tsx
+++ packages/general/src/sections/hooks/use-sync-external-store.tsx
@@ -1,13 +1,50 @@
-import {
-	useEffect,
-	useRef,
-	useState,
-	useSyncExternalStore,
-	useTransition
-} from 'react';
+import { useSyncExternalStore } from 'react';
 
-const Store = (() => {
-	let state = { count: 12 };
+export const UseSyncExternalStore = () => {
+	return (
+		<div>
+			<h4>useSyncExternalStore</h4>
+			<Counter />
+			<CounterDisplay />
+		</div>
+	);
+};
+
+const Counter = () => {
+	const { snapshot, setState } = useStore();
+	return (
+		<button
+			style={{ marginBottom: '1rem' }}
+			onClick={() => setState((old) => ({ value: old.value + 1 }))}
+		>
+			Increment value: {snapshot.value}
+		</button>
+	);
+};
+
+const CounterDisplay = () => {
+	const { snapshot } = useStore();
+	return <div>Value: {snapshot.value}</div>;
+};
+
+// ===============
+
+const ExternalStore = createStore({ value: 0 });
+
+function useStore() {
+	const Store = ExternalStore;
+
+	const snapshot = useSyncExternalStore(
+		Store.subscribe,
+		Store.getSnapshot,
+		Store.getServerSnapshot
+	);
+
+	return { snapshot, setState: Store.setState };
+}
+
+function createStore<T>(initialState: T) {
+	let state = initialState;
 	let listeners = new Set<(newState: typeof state) => void>();
 
 	function snapState() {
@@ -30,72 +67,4 @@ const Store = (() => {
 			return snapState();
 		}
 	};
-})();
-
-export const UseSyncExternalStore = () => {
-	// https://codesandbox.io/s/zl456?file=/src/App.tsx
-	// https://blog.saeloun.com/2021/12/30/react-18-usesyncexternalstore-api
-	// https://www.youtube.com/watch?v=oPfSC5bQPR8&ab_channel=ReactConf2021
-
-	const [, setValue] = useState(0);
-
-	const snapshot = useSyncExternalStore(
-		Store.subscribe,
-		Store.getSnapshot,
-		Store.getServerSnapshot
-	);
-
-	const [isPending, startTransition] = useTransition();
-
-	function measure() {
-		startTransition(() => {
-			setValue((v) => v + 1);
-		});
-	}
-
-	return (
-		<div>
-			<h4>useSyncExternalStore</h4>
-			<button
-				onClick={() => {
-					Store.setState((old) => ({ count: old.count + 1 }));
-				}}
-			>
-				Increment
-			</button>
-			<p>Count: {snapshot.count}</p>
-			<button onClick={measure}>Click and move mouse around</button>
-			<div>
-				{[...Array(5).keys()].map((i) => (
-					<MousePosition key={i} />
-				))}
-			</div>
-		</div>
-	);
-};
-
-function useMousePosition() {
-	const value = useRef({ x: 0, y: 0 });
-
-	useEffect(() => {
-		const handle = (e: MouseEvent) => {
-			value.current = { x: e.clientX, y: e.clientY };
-		};
-
-		window.addEventListener('mousemove', handle);
-		return () => window.removeEventListener('mousemove', handle);
-	}, []);
-
-	return value.current;
 }
-
-const MousePosition = () => {
-	const { x, y } = useMousePosition();
-	const start = performance.now();
-	while (performance.now() - start < 30) {}
-	return (
-		<div>
-			Mouse: {x}-{y}
-		</div>
-	);
-};
--- packages/general/src/sections/hooks/use-transition.tsx
+++ packages/general/src/sections/hooks/use-transition.tsx
@@ -1,6 +1,44 @@
-import { useTransition } from 'react';
+import { memo, useState, useTransition } from 'react';
 
 export const UseTransition = () => {
-	const v = useTransition;
-	return null;
+	const [quickVal, setQuickVal] = useState(0);
+	const [slowVal, setSlowVal] = useState(0);
+	const [isAsync, setAsync] = useState(true);
+	const [, startTransition] = useTransition();
+
+	function handle() {
+		if (isAsync) {
+			setQuickVal((v) => v + 1);
+			startTransition(() => {
+				setSlowVal((v) => v + 1);
+			});
+		} else {
+			setQuickVal((v) => v + 1);
+			setSlowVal((v) => v + 1);
+		}
+	}
+
+	return (
+		<div>
+			<h4>useTransition</h4>
+			<button onClick={handle} style={{ marginRight: '1rem' }}>
+				Increment {quickVal}
+			</button>
+			<button
+				style={{ marginRight: '1rem' }}
+				onClick={() => setAsync((v) => !v)}
+			>
+				{isAsync ? 'Disable async rendering' : 'Enable async rendering'}
+			</button>
+			<MemoizedHeavyComponent val={slowVal} />
+		</div>
+	);
 };
+
+const HeavyComponent = ({ val }: { val: number }) => {
+	const start = performance.now();
+	while (performance.now() - start < 100) {}
+	return <div style={{ marginTop: '1rem' }}>Im so heavy - value: {val}</div>;
+};
+
+const MemoizedHeavyComponent = memo(HeavyComponent);
